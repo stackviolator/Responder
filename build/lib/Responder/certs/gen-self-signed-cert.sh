382b7aba178d338c0b19e2e7b9159b0d93b1e03b
stackviolator <joshmerrill@duck.com>
2024-08-12 15:07:38 -0400

    update gitignore

--- build/lib/Responder/certs/gen-self-signed-cert.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CERT_PATH=$1
-openssl genrsa -out "$CERT_PATH/certs/responder.key" 2048
-openssl req -new -x509 -days 3650 -key "$CERT_PATH/certs/responder.key" -out "$CERT_PATH/certs/responder.crt" -subj "/"
